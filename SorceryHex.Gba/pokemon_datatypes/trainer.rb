dd7bfc230b9da5d6e27921711e56260136c6a5ed
SorcererSoftware@gmail.com <SorcererSoftware@gmail.com>
2014-06-23 23:00:52 -0500

    adding detail to the trainers script
    before the trainers just assumed a nullable pointer. Now it uses an
    array instead, specifying the datatype.
    
    This change would have not been possible under the old system - only
    now, with data available from scripts, is it possible to do dynamic
    sub-structures based on data in the main structure.

--- SorceryHex.Gba/pokemon_datatypes/trainer.rb
+++ SorceryHex.Gba/pokemon_datatypes/trainer.rb
@@ -1,9 +1,28 @@
-﻿#opponentPokemon0: { -ivSpread -level -species -_ }
-#opponentPokemon1: { -ivSpread -level -species -attack -attack -attack -attack -_ }
-#opponentPokemon2: { -ivSpread -level -species -item }
-#opponentPokemon3: { -ivSpread -level -species -item -attack -attack -attack -attack }
+﻿def sharedStruct(b)
+   b.Short "ivSpread"
+   b.ByteNum "level"
+   b.Byte "unused"
+   b.Species
+end
 
-# FR: 23EAC8
+def sharedAttacks(b)
+   b.Short "attack1"; b.Short "attack2"
+   b.Short "attack3"; b.Short "attack4"
+end
+
+trainerStruct0 = ->(b) {
+   sharedStruct(b); b.Short "unused"
+}
+trainerStruct1 = ->(b) {
+   sharedStruct(b); sharedAttacks(b); b.Short "unused"
+}
+trainerStruct2 = ->(b) {
+   sharedStruct(b); b.Short "item"
+}
+trainerStruct3 = ->(b) {
+   sharedStruct(b); sharedAttacks(b); b.Short "item"
+}
+
 trainers = types.FindVariableArray "wwwwwwwwwp", ->(b){
    pokeStructType = b.ByteNum "pokestructure"
    b.Assert (pokeStructType < 4), "pokeStruct must be 0-3"
@@ -18,9 +37,18 @@ trainers = types.FindVariableArray "wwwwwwwwwp", ->(b){
    b.Word "items"
    b.Word "unknown"
 
-   b.ByteNum "pokeCount"
+   count = b.ByteNum "pokeCount"
+   b.Assert (count < 7), "trainers can't have more than 6 pokemon"
    b.Byte "unused"
    b.Short "_"
 
-   b.NullablePointer "opponentPokemon"
+   if pokeStructType == 0
+      b.Array "opponentPokemon", count, trainerStruct0
+   elsif pokeStructType == 1
+      b.Array "opponentPokemon", count, trainerStruct1
+   elsif pokeStructType == 2
+      b.Array "opponentPokemon", count, trainerStruct2
+   else
+      b.Array "opponentPokemon", count, trainerStruct3
+   end
 }
